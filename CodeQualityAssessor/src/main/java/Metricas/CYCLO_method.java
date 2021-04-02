0037c3e6a91121af7125aac280d0a3ab3a66d1ac
1santo <tsvde@iscte-iul.pt>
2021-04-02 20:19:33 +0100

    Metricas\c1

---
+++ CodeQualityAssessor/src/main/java/Metricas/CYCLO_method.java
@@ -0,0 +1,13 @@
+package Metricas;
+
+public class CYCLO_method extends Metrica{
+
+	//quando estiver na classe que Lê files depois instancia-se o valor com o que é passado pelo utilizador na classe GUI
+	
+	public CYCLO_method(int valor) {
+		super("CYCLO_method", valor);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ CodeQualityAssessor/src/main/java/Metricas/ComparadorMetricas.java
@@ -0,0 +1,13 @@
+package Metricas;
+
+import java.util.Comparator;
+
+public class ComparadorMetricas implements Comparator<Metrica> {
+
+//ira ser mais usado e redefinido num Sprint mais à frente
+	public int compare(Metrica metricaLimite, Metrica metricaReal) {
+		// TODO Auto-generated method stub
+		return metricaLimite.getValor()- metricaReal.getValor() ;
+	}
+
+}
